0f4730aff93ed95b6ba304c1d698f7e3959a1249
Charles <cmacanka@gmail.com>
2011-01-17 11:12:17 -0500

    Added comments and sample data.
    Added comments to separate each section of the schema file.  Also added a
    third section that contains some sample data to insert into the database.  This
    is, for now, just inserting some values into the work_types table.

--- schema/paps.sql
+++ schema/paps.sql
@@ -1,10 +1,13 @@
 -- -*- sql -*- --
 
+-- Drop any tables if they already exist.
 DROP TABLE IF EXISTS work_types CASCADE;
 DROP TABLE IF EXISTS works CASCADE;
 DROP TABLE IF EXISTS people CASCADE;
 DROP TABLE IF EXISTS work_authors CASCADE;
 
+-- Create the table.  Required to, of course, create tables that are
+-- referenced before the tables that reference them.
 CREATE TABLE work_types (
   work_type_id SERIAL PRIMARY KEY,
   work_type varchar NOT NULL
@@ -33,3 +36,8 @@ CREATE TABLE work_authors (
   person_id int NOT NULL REFERENCES people (person_id),
   author_position smallint
 );
+
+-- Populate the tables with some example data.
+INSERT INTO work_types (work_type) VALUES ('Textbook');
+INSERT INTO work_types (work_type) VALUES ('Book');
+INSERT INTO work_types (work_type) VALUES ('Paper');
