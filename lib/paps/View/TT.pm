49a7c98d7cf26fbeb2c2531d94cd3255292e29c3
Charles <cmacanka@gmail.com>
2011-01-17 15:16:14 -0500

    Added new template toolkit view named TT.
    New template toolkit view, named TT, created using the built-in paps_create.pl
    script.

---
+++ lib/paps/View/TT.pm
@@ -0,0 +1,36 @@
+package paps::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+=head1 NAME
+
+paps::View::TT - TT View for paps
+
+=head1 DESCRIPTION
+
+TT View for paps.
+
+=head1 SEE ALSO
+
+L<paps>
+
+=head1 AUTHOR
+
+Charles Macanka,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
