524ce9312ab45a4d8bd6c085f1b66d4c611b4b31
Charles <cmacanka@gmail.com>
2011-05-08 13:44:59 -0400

    Added author_position to many_to_many relationship.
    Modified the definition for the many_to_many relationship between the works and
    authors tables to include the work_authors.author_position field in the select
    list.  This makes it much easier (and more efficient) to get the position for
    each author of a particular work, starting from the work row/object.

--- lib/PAPS/Schema/Result/Works.pm
+++ lib/PAPS/Schema/Result/Works.pm
@@ -139,13 +139,42 @@ __PACKAGE__->belongs_to(
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 
+#__PACKAGE__->has_many(
+#  "work_authors",
+#  "PAPS::Schema::Result::WorkAuthors",
+#  { "foreign.work_id" => "self.work_id" },
+#  {
+#      proxy => [ qw/author_position/ ],
+#  },
+#);
+
+# This one doesn't seem to break anything, but does not add anything without a proper proxy
+#__PACKAGE__->has_many(
+#    'work_authors' => 'PAPS::Schema::Result::WorkAuthors',
+#    'work_id',
+#    {
+#        #proxy => [ qw/author_position/ ],
+#        #'+select' => ['author_position'],
+#        #'+as' => ['author_pos'],
+#    },
+#    );
+
 # many_to_many():
 #   args:
 #     1) Name of relationship, DBIC will create accessor with this name
 #     2) Name of has_many() relationship this many_to_many() is shortcut for
 #     3) Name of belongs_to() relationship in model class of has_many() above
 #   You must already have the has_many() defined to use a many_to_many().
-__PACKAGE__->many_to_many(authors => 'work_authors', 'person_id');
+__PACKAGE__->many_to_many(authors => 'work_authors', 'person_id',
+                          {
+                              #proxy => [ qw/author_position/ ],
+                              #'+select' => ['author_position'],
+                              #'+as' => ['author_pos'],
+                              #prefetch => 'work_authors',
+
+                              '+select' => 'me.author_position',
+                              '+as' => 'author_position',
+                          });
 
 
 =head2 display_name
