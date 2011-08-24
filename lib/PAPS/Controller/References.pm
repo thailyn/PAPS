d371e2a51c3f4206631b09597cf993ae2a59877f
Charles <cmacanka@gmail.com>
2011-08-23 22:22:17 -0400

    Added action to list all References.
    The list action in the References controller stores all of the WorkReference
    objects in the stash.  These objects are sorted by the referencing_work_id,
    chapter, and rank.  Currently, we can not sort by the title or display name of
    the referencing works, as that would require joining on the works table for
    each Reference.  Will need to optimize these queries to be able to do that.

--- lib/PAPS/Controller/References.pm
+++ lib/PAPS/Controller/References.pm
@@ -28,6 +28,26 @@ sub index :Path :Args(0) {
 }
 
 
+=head2 list
+
+Fetch all Reference objects and pass to references/list.tt2 in the stash to be displayed
+
+=cut
+
+sub list :Local {
+    my ($self, $c) = @_;
+
+    # Retrieve all of the Reference records as WorkReference model objects and
+    # store in the stash where they can be accessed by the TT template
+    $c->stash(references => [$c->model('DB::WorkReference')
+                             ->search(undef, { order_by => { -asc => [qw/referencing_work_id chapter rank/] }, })
+                             ->all]);
+
+    # Set the TT template to use.
+    $c->stash(template => 'references/list.tt2');
+}
+
+
 =head1 AUTHOR
 
 Charles Macanka,,,
