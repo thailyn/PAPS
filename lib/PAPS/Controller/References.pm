5d587f1e638f5e76007553dab8f6d2f656a7563a
Charles <cmacanka@gmail.com>
2011-08-23 23:00:59 -0400

    Added edit_form and do_edit actions for References.
    As pointed out in the POD for the do_edit action, the logic to edit a
    Reference here is virtually identical to how References are updated in the
    Works controller.  The logic to do this should be moved into a single
    location.

--- lib/PAPS/Controller/References.pm
+++ lib/PAPS/Controller/References.pm
@@ -44,6 +44,58 @@ sub base :Chained('/') :PathPart('references') :CaptureArgs(0) {
 }
 
 
+=head2 edit_form
+
+=cut
+
+sub edit_form :Chained('base') :PathPart('edit') :Args(0) {
+    my ($self, $c) = @_;
+
+    # Retrieve all of the Reference records as WorkReference model objects and
+    # store in the stash where they can be accessed by the TT template
+    $c->stash(references => [$c->model('DB::WorkReference')
+                             ->search(undef, { order_by => { -asc => [qw/referencing_work_id chapter rank/] }, })
+                             ->all]);
+
+    # Set the TT template to use
+    $c->stash(template => 'references/edit.tt2');
+}
+
+=head2 do_edit
+
+This method is virtually identical to do_edit_reference in the Works
+controller.  Figure out which to keep and get rid of one, so we do not
+duplicate code.
+
+=cut
+
+sub do_edit :Chained('base') :PathPart('do_edit') :Args(1) {
+    my ($self, $c, $reference_id) = @_;
+
+    if (lc $c->request->method ne 'post') {
+        return;
+    }
+
+    my $params = $c->request->params;
+    my $reference = $c->model('DB::WorkReference')->find({id => {'=', $reference_id}});
+
+    if (lc $params->{submit} eq 'edit') {
+        $reference->update({
+            referencing_work_id => $params->{referencing_work_id} || undef,
+            rank => $params->{rank} || undef,
+            chapter => $params->{chapter} || undef,
+            reference_type_id => $params->{reference_type_id} || undef,
+            referenced_work_id => $params->{referenced_work_id} || undef,
+            reference_text => $params->{reference_text} || undef,
+                           });
+    }
+
+    return $c->res->redirect(
+        $c->uri_for($c->controller('references')->action_for('edit_form'),
+                    [ ]));
+}
+
+
 =head2 list
 
 Fetch all Reference objects and pass to references/list.tt2 in the stash to be displayed
