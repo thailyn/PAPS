7cb536ca1da86ae8695784a934bb74ff2c5d41b0
Charles <cmacanka@gmail.com>
2011-12-26 21:59:50 -0500

    Updated version number to 0.009.

--- lib/PAPS.pm
+++ lib/PAPS.pm
@@ -22,7 +22,7 @@ use lib "$ENV{HOME}/projects/PAPS-Database-papsdb-Schema/lib";
 
 extends 'Catalyst';
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 $VERSION = eval $VERSION;
 
 # Configure the application.
