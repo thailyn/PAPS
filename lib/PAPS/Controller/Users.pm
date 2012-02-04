908384f6deefd46fed3523b54fb7799fd9bcf0f7
Charles <cmacanka@gmail.com>
2012-02-04 14:12:38 -0500

    Added base, user, and list actions and a Users list view.

--- lib/PAPS/Controller/Users.pm
+++ lib/PAPS/Controller/Users.pm
@@ -27,6 +27,59 @@ sub index :Path :Args(0) {
     $c->response->body('Matched PAPS::Controller::Users in Users.');
 }
 
+=head2 base
+
+Can place common logic to start chained dispatch here
+
+=cut
+
+sub base :Chained('/') :PathPart('users') :CaptureArgs(0) {
+    my ($self, $c) = @_;
+
+    # Store the ResultSet in stash so it's available for other methods
+    $c->stash(users_rs => $c->model('DB::User'));
+
+    ## Print a message to the debug log
+    #$c->log->debug('*** INSIDE BASE USERS METHOD ***');
+}
+
+=head2 user
+
+Captures an argument indicating a specific user_id to deal with.  If the
+referenced user exists, it and its user_id are stored in the stash.  If the
+user does not exist, the method dies.
+
+=cut
+
+sub work :Chained('base'): PathPart('') :CaptureArgs(1) {
+    my ($self, $c, $user_id) = @_;
+
+    my $user = $c->stash->{users_rs}->find({ id => $user_id },
+                                           { key => 'primary' });
+
+    die "No such user" unless ($user);
+
+    $c->stash(user_id => $user_id);
+    $c->stash(user => $user);
+}
+
+=head2 list
+
+Fetch all user objects and pass to users/list.tt2 in stash to be displayed
+
+=cut
+
+sub list :Local {
+    my ($self, $c) = @_;
+
+    # Retrieve all of the user records as user model objects and store in the
+    # stash where they can be accessed by the TT template
+    $c->stash(users => [$c->model('DB::User')->all]);
+
+    # Set the TT template to use.
+    $c->stash(template => 'users/list.tt2');
+}
+
 
 =head1 AUTHOR
 
